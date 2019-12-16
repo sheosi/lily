5ee0d6cefb5f0549e0266c7dc79429d6f64b32a0
sheosi <sertorbe@gmail.com>
2019-12-16 19:28:27 +0100

    Remove files from last commit

--- python/lily_sdk/__init__.py
+++
@@ -1,8 +0,0 @@
-action_classes = {}
-
-def action(name):
-	def inner_deco(cls):
-		action_classes[name] = cls
-		return cls
-
-	return inner_deco
--- python/test/__init__.py
+++
@@ -1,7 +0,0 @@
-import lily_sdk
-import lily
-	
-@lily_sdk.action(name = "say")
-class Say():
-    def trigger_action(args):
-        lily.say(args)
