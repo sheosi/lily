7097686e17707c052760eab77959b8c7efd7fbff
sheosi <sertorbe@gmail.com>
2020-07-14 01:39:05 +0200

    This should've been in last commit

--- packages/default/python/test/__init__.py
+++
@@ -1,7 +0,0 @@
-from lily_ext import action, translate, answer
-import datetime
-
-@action(name = "say_date_time")
-class SayTime():
-    def trigger_action(self,args, _context):
-        answer(datetime.datetime.now().strftime(translate(args, {})))
