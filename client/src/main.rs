a8ecd76541915803f8b7ed1f9e519ce7a6827d20
sheosi <sertorbe@gmail.com>
2021-05-18 22:23:00 +0200

    Put active listening up again (helps with the ogg opus problems)

--- client/src/main.rs
+++ client/src/main.rs
@@ -32,7 +32,7 @@ const CONN_CONF_FILE: MultipathRef = MultipathRef::new(&[
 
 const SNOWBOY_DATA_PATH: PathRef = PathRef::own("hotword");
 pub const HOTWORD_CHECK_INTERVAL_MS: u16 = 20; // Larger = less CPU, more wait time
-pub const ACTIVE_LISTENING_INTERVAL_MS: u16 = 50; 
+pub const ACTIVE_LISTENING_INTERVAL_MS: u16 = 200; 
 enum ProgState<'a> {
     PasiveListening,
     ActiveListening(AsyncMGuard<'a, RecDevice>),
